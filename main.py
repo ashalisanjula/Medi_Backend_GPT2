3d1a719ed0e4c09e3e46695ed7e27f142e141e20
Ashali Samaraweera <ashalisanjula@gmail.com>
2024-12-09 14:45:37 +0530

    giving a endpoint to check service

--- main.py
+++ main.py
@@ -35,6 +35,11 @@ def generate_ai_suggestions(input_text: str, num_suggestions: int = 3) -> List[s
     generated = text_gen_model(input_text, max_length=20, num_return_sequences=num_suggestions, num_beams=3)
     return [g['generated_text'].strip() for g in generated]
 
+# Endpoint to check if the server is running
+@app.route('/', methods=['GET'])
+def status_check():
+    
+    return JSONResponse({'prompt': "Server is Up and Running..."})
 
 @app.route('/suggest', methods=["POST"])
 async def suggest(request: Request):
